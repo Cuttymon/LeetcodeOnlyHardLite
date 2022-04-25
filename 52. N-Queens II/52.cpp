d58bb9b9d4d18926bbf52507b067837f88c0718f
Cuttymon <qq1138331845123@outlook.com>
2022-04-25 16:42:28 +0800

    ok

---
+++ 52. N-Queens II/52.cpp
@@ -0,0 +1,48 @@
+class Solution
+{
+public:
+    int totalNQueens(int n)
+    {
+        vector<string> board(n, string(n, '.')); //初始化nxn皇后棋盘
+        int res = 0;                             //初始化计数器
+        backtrack(board, 0, res);
+        return res;
+    }
+    void backtrack(vector<string> &board, int row, int &res)
+    {
+        if (row == board.size()) //遍历完成，结束
+        {
+            res += 1;
+            return;
+        }
+        for (int col = 0; col < board.size(); col++)
+        {
+            if (isValid(board, row, col)) //判断是否合法
+            {
+                board[row][col] = 'Q';          //放置皇后
+                backtrack(board, row + 1, res); //递归回溯
+                board[row][col] = '.';          //穷举到底后并将此方案计入数量后，撤销选择，恢复原来的状态
+            }
+        }
+    }
+
+    bool isValid(vector<string> &board, int row, int col)
+    {
+        for (int i = 0; i < row; i++) //判断是否在同一列
+        {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) //左上的延伸查看有无皇后
+        {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.size(); i--, j++) //右上的延伸查看有无皇后
+        {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+        return true;
+    }
+};
